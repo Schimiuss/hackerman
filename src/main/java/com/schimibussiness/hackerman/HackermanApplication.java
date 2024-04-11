67225466f6d931575bf02aa78e2fb4151041883a
Schimiuss <schimiuss@gmail.com>
2024-04-11 18:42:56 +0200

    first commit

---
+++ src/main/java/com/schimibussiness/hackerman/HackermanApplication.java
@@ -0,0 +1,13 @@
+package com.schimibussiness.hackerman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackermanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackermanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schimibussiness/hackerman/HackermanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schimibussiness.hackerman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackermanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
